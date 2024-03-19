dbac2764f1b1f7f432f3b341a1ca4ed3f815bbc1
amirhossein hajitabar <amirhajitabar2@gmail.com>
2024-03-19 14:40:46 +0330

    add dashboard sidebar and routing

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useLocation } from "react-router-dom"
+import DashSidebar from '../components/DashSidebar'
+import DashProfile from '../components/DashProfile'
 
 export default function Dashboard() {
+    const location = useLocation()
+    const [tab, setTab] = useState('')
+    useEffect(() => {
+        const urlParams = new URLSearchParams(location.search)
+        const tabFromUrl = urlParams.get('tab')
+        if (tabFromUrl) setTab(tabFromUrl)
+    }, [location.search])
     return (
-        <div>Dashboard</div>
+        <div>
+            <div>
+                {/* sidebar */}
+                <DashSidebar />
+            </div>
+            <div>
+                {/* rightbar */}
+                {tab === 'profile' && <DashProfile />}
+            </div>
+        </div>
     )
 }
