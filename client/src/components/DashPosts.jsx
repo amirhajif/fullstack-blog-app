9460aca74c76d7132b48adb418f82e94573adc08
amirhossein hajitabar <amirhajitabar2@gmail.com>
2024-03-29 15:36:14 +0330

    add scorllbar tailwind css package and config

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -1,6 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
 
 export default function DashPosts() {
+    const { currentUser } = useSelector((state) => state.user)
+    const [posts, setPosts] = useState([])
+    console.log(posts)
+    useEffect(() => {
+        const fetchPosts = async () => {
+            try {
+                const res = await fetch(`/api/post/getposts?userId=${currentUser._id}`)
+                const data = await res.json()
+                if (res.ok) setPosts(data.posts)
+
+            } catch (err) {
+                console.log(err)
+            }
+        }
+        if (currentUser.isAdmin) {
+            fetchPosts()
+        }
+    }, [currentUser._id])
     return (
         <div>DashPosts</div>
     )
