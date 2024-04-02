9748ce9992fb46b53413b0ed48debe2b868dafea
amirhossein hajitabar <amirhajitabar2@gmail.com>
2024-04-02 13:23:07 +0330

    complete call to action

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -1,6 +1,7 @@
 import { Button, Spinner } from 'flowbite-react';
 import React, { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
+import CallToAction from '../components/CallToAction';
 export default function PostPage() {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(false);
@@ -63,7 +64,9 @@ export default function PostPage() {
             <div
                 className='p-3 max-w-2xl mx-auto w-full post-content'
                 dangerouslySetInnerHTML={{ __html: post && post.content }}>
-
+            </div>
+            <div className='max-w-4xl mx-auto w-full'>
+                <CallToAction />
             </div>
         </main>
     )
