25d539d6055359551cd68dfcdf8bcbf10739e91d
amirhossein hajitabar <amirhajitabar2@gmail.com>
2024-04-04 00:30:04 +0330

    complete like functionality for comment of a post

--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -1,7 +1,7 @@
 import { Alert, Button, Textarea } from 'flowbite-react'
 import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom';
 import Comment from './Comment'
 
 export default function CommentSection({ postId }) {
@@ -11,6 +11,9 @@ export default function CommentSection({ postId }) {
     const [comments, setComments] = useState([]);
     const [showModal, setShowModal] = useState(false);
     const [commentToDelete, setCommentToDelete] = useState(null);
+
+    const navigate = useNavigate();
+
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
@@ -51,6 +54,34 @@ export default function CommentSection({ postId }) {
         };
         getComments();
     }, [postId]);
+
+    const handleLike = async (commentId) => {
+        try {
+            if (!currentUser) {
+                navigate('/sign-in');
+                return;
+            }
+            const res = await fetch(`/api/comment/likeComment/${commentId}`, {
+                method: 'PUT',
+            });
+            if (res.ok) {
+                const data = await res.json();
+                setComments(
+                    comments.map((comment) =>
+                        comment._id === commentId
+                            ? {
+                                ...comment,
+                                likes: data.likes,
+                                numberOfLikes: data.likes.length,
+                            }
+                            : comment
+                    )
+                );
+            }
+        } catch (error) {
+            console.log(error.message);
+        }
+    };
     return (
         <div className='max-w-2xl mx-auto w-full p-3'>
             {
@@ -115,7 +146,7 @@ export default function CommentSection({ postId }) {
                         <Comment
                             key={comment._id}
                             comment={comment}
-                        // onLike={handleLike}
+                            onLike={handleLike}
                         // onEdit={handleEdit}
                         // onDelete={(commentId) => {
                         //     setShowModal(true);
