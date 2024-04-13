4b7b94311afe57bbc9807b67780cc7555d723f05
amirhossein hajitabar <amirhajitabar2@gmail.com>
2024-04-13 15:06:24 +0330

    add dashboard tab and component and route

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import { Sidebar } from 'flowbite-react'
-import { HiAnnotation, HiArrowSmRight, HiDocumentText, HiOutlineUserGroup, HiUser } from 'react-icons/hi'
 import { Link, useLocation } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
+import { HiAnnotation, HiArrowSmRight, HiChartPie, HiDocumentText, HiOutlineUserGroup, HiUser } from 'react-icons/hi'
 import { signoutSuccessfull } from '../redux/user/userSlice'
 import { useSelector } from 'react-redux'
 
@@ -40,6 +40,17 @@ export default function DashSidebar() {
         <Sidebar className='w-full md:w-56'>
             <Sidebar.Items>
                 <Sidebar.ItemGroup className='flex flex-col gap-1'>
+                    {currentUser && currentUser.isAdmin && (
+                        <Link to='/dashboard?tab=dash'>
+                            <Sidebar.Item
+                                active={tab === 'dash' || !tab}
+                                icon={HiChartPie}
+                                as='div'
+                            >
+                                Dashboard
+                            </Sidebar.Item>
+                        </Link>
+                    )}
                     <Link to='/dashboard?tab=profile'>
                         <Sidebar.Item as='div' active={tab === 'profile'} icon={HiUser} label={currentUser.isAdmin ? 'Admin' : 'User'} labelColor="dark" >
                             Profile
